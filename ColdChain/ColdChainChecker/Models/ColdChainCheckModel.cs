45072646ce20193bf62a5b0fa891bc1fe1ef065b
Alejandro Martínez <81448316+jalejomg@users.noreply.github.com>
2021-11-30 11:53:33 -0500

    chore: create model

---
+++ ColdChain/ColdChainChecker/Models/ColdChainCheckModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ColdChainChecker.Models
+{
+    public class ColdChainCheckModel
+    {
+        public int? Gps { get; set; }
+        public int? Humidity { get; set; }
+        public int? Temperature { get; set; }
+        public int? DoorOpenTimes { get; set; }
+        public int? DeviceCharge { get; set; }
+        public bool? ContractFulfilled { get; set; }
+    }
+}
